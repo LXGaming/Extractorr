49bb009b12fb042f41e6bc002b15b2a76a363027
Alex Thomson <LXGaming@users.noreply.github.com>
2022-03-29 16:34:52 +1300

    Bump version

--- LXGaming.Extractorr.Server/Utilities/Constants.cs
+++ LXGaming.Extractorr.Server/Utilities/Constants.cs
@@ -6,7 +6,7 @@ public static class Constants {
 
         public const string Id = "extractorr";
         public const string Name = "Extractorr";
-        public const string Version = "1.0.11";
+        public const string Version = "1.0.12";
         public const string Authors = "LX_Gaming";
         public const string Source = "https://github.com/LXGaming/Extractorr";
         public const string Website = "https://lxgaming.github.io/";
