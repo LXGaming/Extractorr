ffa3e67b02e54f7c536df34b3f51579bd22beb31
Alex Thomson <10443061+LXGaming@users.noreply.github.com>
2024-01-09 16:16:51 +1300

    Update Flood models

--- LXGaming.Extractorr.Server/Services/Flood/Models/TorrentContent.cs
+++ LXGaming.Extractorr.Server/Services/Flood/Models/TorrentContent.cs
@@ -4,17 +4,17 @@ namespace LXGaming.Extractorr.Server.Services.Flood.Models;
 
 public record TorrentContent {
 
-    [JsonPropertyName("filename")]
-    public string? Filename { get; init; }
-
     [JsonPropertyName("index")]
     public int Index { get; init; }
 
     [JsonPropertyName("path")]
-    public string? Path { get; init; }
+    public required string Path { get; init; }
+
+    [JsonPropertyName("filename")]
+    public required string Filename { get; init; }
 
     [JsonPropertyName("percentComplete")]
-    public decimal? PercentComplete { get; init; }
+    public double PercentComplete { get; init; }
 
     [JsonPropertyName("priority")]
     public TorrentContentPriority Priority { get; init; }
