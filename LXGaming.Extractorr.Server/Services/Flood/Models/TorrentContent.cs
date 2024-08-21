fb0bc07bfa4c0f21b5bb9b2d2e1c56f0ba9b9ee1
Alex Thomson <10443061+LXGaming@users.noreply.github.com>
2024-08-21 20:23:36 +1200

    Add reference to Flood models

--- LXGaming.Extractorr.Server/Services/Flood/Models/TorrentContent.cs
+++ LXGaming.Extractorr.Server/Services/Flood/Models/TorrentContent.cs
@@ -2,6 +2,7 @@
 
 namespace LXGaming.Extractorr.Server.Services.Flood.Models;
 
+// https://github.com/jesec/flood/blob/7cdf1de10743f6f4bf5f0eb553450f8e2a60e268/shared/types/TorrentContent.ts#L7
 public record TorrentContent {
 
     [JsonPropertyName("index")]
--- LXGaming.Extractorr.Server/Services/Flood/Models/TorrentContentPriority.cs
+++ LXGaming.Extractorr.Server/Services/Flood/Models/TorrentContentPriority.cs
@@ -1,5 +1,6 @@
 ﻿namespace LXGaming.Extractorr.Server.Services.Flood.Models;
 
+// https://github.com/jesec/flood/blob/7cdf1de10743f6f4bf5f0eb553450f8e2a60e268/shared/types/TorrentContent.ts#L1
 public enum TorrentContentPriority {
 
     DoNotDownload = 0,
